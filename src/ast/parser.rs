afcc44c3f5516b0af25752f78c0794a67866e88e
BlueTheDuck <hello@perezv.ar>
2023-10-10 23:57:18 -0300

    Ast handles assignment

--- src/ast/parser.rs
+++ src/ast/parser.rs
@@ -1,18 +1,18 @@
 use nom::{
     branch::alt,
     bytes::complete::take,
-    combinator::{map, map_opt, verify},
+    combinator::{map, map_opt, verify, opt},
     multi::many0,
-    sequence::{terminated, tuple},
+    sequence::{terminated, tuple, preceded},
     IResult,
 };
 
 use crate::lexer::{
     stream::TokenStream,
-    token::{Ident, Token},
+    token::{Ident, Literal, Token},
 };
 
-use super::tree::{FuncDecl, Statement};
+use super::tree::{FuncDecl, Statement, VarDecl};
 
 fn open_paren_tag<'i>(input: TokenStream<'i>) -> IResult<TokenStream<'i>, TokenStream<'i>> {
     verify(take(1usize), |t: &TokenStream| t[0] == Token::OpenParen)(input)
@@ -26,14 +26,37 @@ fn open_brace_tag<'i>(input: TokenStream<'i>) -> IResult<TokenStream<'i>, TokenS
 fn close_brace_tag<'i>(input: TokenStream<'i>) -> IResult<TokenStream<'i>, TokenStream<'i>> {
     verify(take(1usize), |t: &TokenStream| t[0] == Token::CloseBrace)(input)
 }
+fn assign_tag<'i>(input: TokenStream<'i>) -> IResult<TokenStream<'i>, TokenStream<'i>> {
+    verify(take(1usize), |t: &TokenStream| t[0] == Token::Assign)(input)
+}
+fn semi_colon_tag<'i>(input: TokenStream<'i>) -> IResult<TokenStream<'i>, TokenStream<'i>> {
+    verify(take(1usize), |t: &TokenStream| t[0] == Token::SemiColon)(input)
+}
 fn eof_tag<'i>(input: TokenStream<'i>) -> IResult<TokenStream<'i>, TokenStream<'i>> {
     verify(take(1usize), |t: &TokenStream| t[0] == Token::Eof)(input)
 }
 
-fn parse_ident<'i>(i: TokenStream<'i>) -> IResult<TokenStream<'i>, Ident> {
+pub(super) fn parse_ident<'i>(i: TokenStream<'i>) -> IResult<TokenStream<'i>, Ident> {
     map_opt(take(1usize), |t: TokenStream| {
         t.tokens[0].as_ident().copied()
     })(i)
+}
+
+fn parse_literal<'i>(i: TokenStream<'i>) -> IResult<TokenStream<'i>, Literal> {
+    map_opt(take(1usize), |t: TokenStream| {
+        t.tokens[0].as_literal().copied()
+    })(i)
+}
+
+fn parse_var_decl<'i>(input: TokenStream<'i>) -> IResult<TokenStream, VarDecl> {
+    map(
+        tuple((parse_ident, parse_ident, opt(preceded(assign_tag, parse_literal)), semi_colon_tag)),
+        |(ty, name, value, _)| VarDecl {
+            ty,
+            name,
+            value,
+        },
+    )(input)
 }
 
 fn parse_fn<'i>(input: TokenStream<'i>) -> IResult<TokenStream, FuncDecl> {
@@ -44,20 +67,23 @@ fn parse_fn<'i>(input: TokenStream<'i>) -> IResult<TokenStream, FuncDecl> {
             open_paren_tag,
             close_paren_tag,
             open_brace_tag,
+            many0(parse_statement),
             close_brace_tag,
         )),
-        |(ty, name, _, _, _, _)| FuncDecl {
+        |(ty, name, _, _, _, body, _)| FuncDecl {
             ret: ty,
             name,
-            body: None,
+            body,
         },
     )(input)
 }
 
 fn parse_statement<'i>(input: TokenStream<'i>) -> IResult<TokenStream<'i>, Statement<'i>> {
-    alt((map(parse_fn, Statement::FuncDecl), |i: TokenStream| {
-        panic!("Unimplemented: {:?}", i.tokens)
-    }))(input)
+    alt((
+        map(parse_fn, Statement::FuncDecl),
+        map(parse_var_decl, Statement::VarDecl),
+        /* |i: TokenStream| { panic!("Unimplemented: {:?}", i.tokens) } */
+    ))(input)
 }
 
 pub fn parse_stream<'i>(tokens: TokenStream<'i>) -> Vec<Statement<'i>> {
--- src/ast/tree.rs
+++ src/ast/tree.rs
@@ -1,30 +1,66 @@
 use std::fmt::Display;
 
-use crate::lexer::token::Ident;
+use crate::lexer::token::{Ident, Literal};
 
+#[derive(Debug, PartialEq, Eq)]
 pub struct FuncDecl<'i> {
     pub ret: Ident<'i>,
     pub name: Ident<'i>,
-    pub body: Option<Vec<Statement<'i>>>,
+    pub body: Vec<Statement<'i>>,
 }
 impl<'i> Display for FuncDecl<'i> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{} {}() {{ ", self.ret, self.name)?;
-        for stmt in self.body.iter().flatten() {
-            write!(f, "{stmt} ")?;
+        write!(f, "{} {}() {{\n", self.ret, self.name)?;
+        for stmt in self.body.iter() {
+            write!(f, "{stmt} \n")?;
         }
-        write!(f, "}}")
+        write!(f, "}}\n")
     }
 }
 
+#[derive(Debug, PartialEq, Eq)]
+pub struct VarDecl<'i> {
+    pub ty: Ident<'i>,
+    pub name: Ident<'i>,
+    pub value: Option<Literal>,
+}
+
+#[derive(Debug, PartialEq, Eq)]
 pub enum Statement<'i> {
     FuncDecl(FuncDecl<'i>),
+    VarDecl(VarDecl<'i>),
+}
+
+impl<'i> Statement<'i> {
+    pub const fn new_func_decl(ret: Ident<'i>, name: Ident<'i>, body: Vec<Statement<'i>>) -> Self {
+        Self::FuncDecl(FuncDecl {
+            ret,
+            name,
+            body,
+        })
+    }
+    pub const fn new_var_decl(ty: Ident<'i>, name: Ident<'i>, value: Option<Literal>) -> Self {
+        Self::VarDecl(VarDecl {
+            ty,
+            name,
+            value,
+        })
+    }
 }
 
 impl<'i> Display for Statement<'i> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
             Statement::FuncDecl(func) => write!(f, "{func}"),
+            Statement::VarDecl(var) => {
+                write!(f, "{ty} {name}", ty = var.ty, name = var.name)?;
+                if let Some(value) = var.value {
+                    write!(f, " = {value}")?;
+                }
+                write!(f, ";")?;
+                Ok(())
+            },
+            
         }
     }
 }

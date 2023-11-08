04d51d0065c0abd1206940af48958e24c03c3e7c
BlueTheDuck <hello@perezv.ar>
2023-11-08 03:12:55 -0300

    Add C files to test

---
+++ example/phase_2.c
@@ -0,0 +1,5 @@
+#define X 1 + \
+        2 + \
+        3
+static char* x = "hello \
+world";
---
+++ example/typedef.c
@@ -0,0 +1,4 @@
+typedef int int32_t;
+typedef unsigned int uint32_t;
+typedef unsigned char uint8_t;
+typedef unsigned short uint16_t;
